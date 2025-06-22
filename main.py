5fb1f613674e18b28a9b25850642d1de0326ea36
chanduranwala <chanduranwala@gmail.com>
2025-06-22 07:26:13 +0200

    Add on_connect to state management.

--- main.py
+++ main.py
@@ -1,32 +1,35 @@
-from nicegui import ui
+from nicegui import app, ui
 import datetime
 import pytz
-import os
 
 time_zones = pytz.all_timezones
-
-today = datetime.date.today()
-time = datetime.datetime.now().time()
 
 def on_button_click():
     date_value = None
     time_value = None
 
-    if isinstance(selected_date.value, str):
-        date_value = datetime.date.fromisoformat(selected_date.value)
+    if not selected_date.value or not selected_time.value:
+        original_time_zone_label.text = "Please select a date and time"
 
-    if isinstance(selected_time.value, str):
-        time_value = datetime.time.fromisoformat(selected_time.value)
+    try:
+        if isinstance(selected_date.value, str):
+            date_value = datetime.date.fromisoformat(selected_date.value)
 
-    dt = datetime.datetime.combine(date_value, time_value)
+        if isinstance(selected_time.value, str):
+            time_value = datetime.time.fromisoformat(selected_time.value)
 
-    input_tz = pytz.timezone(from_zone.value)
-    localize_dt = input_tz.localize(dt)
+        dt = datetime.datetime.combine(date_value, time_value)
 
-    output_tz = pytz.timezone(to_zone.value)
-    converted_dt = dt.astimezone(output_tz)
+        input_tz = pytz.timezone(from_zone.value)
+        localize_dt = input_tz.localize(dt)
 
-    result_label.text = f"🕓 {from_zone.value}: {localize_dt} → {to_zone.value}: {converted_dt}"
+        output_tz = pytz.timezone(to_zone.value)
+        converted_dt = dt.astimezone(output_tz)
+
+        original_time_zone_label.text = f"🕓 {from_zone.value}: {localize_dt}"
+        converted_time_zone_label.text = f"🌎 {to_zone.value}: {converted_dt}"
+    except Exception as e:
+        original_time_zone_label.text = f"Something wrong here {e}"
 
 
 with ui.header().classes('bg-blue-500 text-white'):
@@ -36,8 +39,8 @@ with ui.column().classes('items-center w-auto mx-auto gap-10 mt-20'):
     ui.label("⏱️ Date Time Converter").tailwind('font-bold', 'text-4xl')
 
     with ui.row().classes('justify-center items-center gap-4'):
-        selected_date = ui.date(value=str(today))
-        selected_time = ui.time(value=str(time))
+        selected_date = ui.date()
+        selected_time = ui.time()
 
     with ui.row().classes('gap-4'):
         with ui.column().classes('gap-0'):
@@ -48,8 +51,17 @@ with ui.column().classes('items-center w-auto mx-auto gap-10 mt-20'):
             ui.label("To Time Zone").tailwind('font-bold')
             to_zone = ui.select(time_zones, value="Asia/Colombo")
 
-    result_label = ui.label("")
+    original_time_zone_label = ui.label("")
+    converted_time_zone_label = ui.label("")
 
     ui.button("Convert Time", on_click=on_button_click)
+
+    def handle_state():
+        now = datetime.datetime.now()
+        selected_date.value = now.date().isoformat()
+        selected_time.value = now.time().strftime('%H:%M:%S')
+
+    app.on_connect(handle_state)
+
 
 ui.run()
